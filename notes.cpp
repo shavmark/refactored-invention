bba65787d1fb7ece60e735a2f25821347b937361
shavmark <mark@shavlik.us>
2016-04-28 17:26:08 -0500

    ready for final .1 testing

--- notes.cpp
+++ notes.cpp
@@ -1,5 +1,6 @@
 // store notes here
 // items to help make it more human https://dev.projectoxford.ai/docs/services/563309b6778daf02acc0a508/operations/563309b7778daf06340c9652
+//https://processing.org/tutorials/transform2d/
 // https://github.com/CNMAT/OSC arduino osc, good stuff to get that thing going
 //http://www.jamesalliban.com/#jahome good site for learning the industry
 //http://jpbellona.com/kinect/ cool notes
