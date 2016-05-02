d1c0e2cd5634e79394ab699f10ce1a8c5765824a
mark <mark@shavlik.us>
2016-05-02 11:35:04 -0500

    aaargh

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,21 @@
 //========================================================================
 int main(int argc, char *argv[]){
 
-	ofApp *app = new ofApp(); // assume ram here or just blow up any way
+
 
 	vector<string> arguments = vector<string>(argv, argv + argc);
+	bool findkinect = false;
 	for (const auto& arg : arguments) {
 		if (arg.find("kinect")) {
-			app->seekKinect = true; // command line
+			findkinect = true;
 		}
 	}
+	// let command line get processed before we allocated to avoid depend. issues
 
 	Software2552::init(); //bugbug maybe pass opengl and other parmeters here
-
+	ofApp *app = new ofApp(); // assume ram here or just blow up any way
+	app->seekKinect = findkinect; // command line
+							
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
